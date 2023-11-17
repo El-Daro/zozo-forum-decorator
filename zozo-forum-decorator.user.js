4f345984a8fda1c7480c1e73e76bb31a054b70fc
El Daro <el_daro@outlook.com>
2023-11-17 15:17:10 +0300

    Update zozo-forum-decorator.user.js
    Fixed the update tag

--- zozo-forum-decorator.user.js
+++ zozo-forum-decorator.user.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name         ZOZO Forum Decorator
 // @namespace    https://github.com/El-Daro/zozo-forum-decorator
-// @version      0.1
+// @version      0.2
 // @description  Makes some utility fonts readable for the ZOZO forum.
 // @author       El Daro
 // @match        http*://forum.zozo.gg/*
 // @grant        GM_addStyle
 // @run-at       document-start
-// @updateURL    https://github.com/El-Daro/zozo-forum-decorator/zozo-forum-decorator.user.js
-// @downloadURL  https://github.com/El-Daro/zozo-forum-decorator/zozo-forum-decorator.user.js
+// @updateURL    https://github.com/El-Daro/zozo-forum-decorator/raw/main/zozo-forum-decorator.user.js
+// @downloadURL  https://github.com/El-Daro/zozo-forum-decorator/raw/main/zozo-forum-decorator.user.js
 // ==/UserScript==
 
 // CSS
-//  'Deleted by' text
+//  'Deleted by' text (#334734)
 GM_addStyle (`
     .messageNotice.messageNotice--deleted {
-        color: #334734 !important;
+        color: #00ff00 !important;
     }
 `);
